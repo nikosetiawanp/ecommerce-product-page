533ba21a40f58845932702fa7187736a5f759617
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-17 11:20:01 +0700

    update github workflows

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/ecommerce-product-page",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
 module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({

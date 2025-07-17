7e635d886b8765d24a7f4f2b9580feae2ec173df
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-17 15:52:25 +0700

    add gray-matter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+type Cat = {
+  name: string;
+  description: string;
+};
+
+const filePath = path.join(process.cwd(), "contents/home.md");
+const fileContent = fs.readFileSync(filePath, "utf8");
+const { data: frontmatter, content } = matter(fileContent);
+
 export default function Home() {
   return (
     <main>
       <div>Next.js on GitHub Pages</div>
+      <p>{frontmatter.title}</p>
+      <ul>
+        {frontmatter.cats.map((cat: Cat, index: number) => (
+          <li key={index}>
+            <b>{cat.name}</b> - {cat.description}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }

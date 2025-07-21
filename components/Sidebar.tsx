63f595a263c9609064e571397898fb829be3df96
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-21 16:27:56 +0700

    refactor svg on QuantityAddToCart.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar() {
           <path
             d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
             fill="#69707D"
-            fill-rule="evenodd"
+            fillRule="evenodd"
           />
         </svg>{" "}
       </button>
@@ -32,7 +32,7 @@ export default function Sidebar() {
                 <path
                   d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
                   fill="#69707D"
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                 />
               </svg>
             </button>
